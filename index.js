14ff2b8505038b3e8834fce31974c1aec2f0ff4a
David <soydoft@gmail.com>
2024-06-05 23:39:14 -0400

    fixed get and post requests

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const database = [5];
 app.use(express.json()); //enable JSON parse in request body
 
 app.get('/api/data' , (req, res) => {
-    if (!database) res.status(404).send('Error getting database')
+    if (!database) res.status(404).send('Error getting database');
     res.send(database);
 })
 
 app.post('/api/data' , (req, res) => {
-    if (!database) res.status(404).send('Error getting database')
-    else res.send(database);
+    const data = req.body.data;
+    if (!data) res.status(404).send('Error getting data');
+    database.push(parseInt(data, 10));
+    res.send(database);
 })
 
 app.put('/api/data/:element' , (req, res) => {
     const index = database.indexOf(req.params.element);
-    if (index === -1) {
-        res.status(404).send('Error putting data')
-    }
+    if (index === -1) res.status(404).send('data does not exist');
     else {
-        database[index] = req.body.value;
+        database[index] = parseInt(req.body.data, 10);
         res.send(database[index]);
     }
 })
 
 app.delete('/api/data/:element' , (req, res) => {
-    if (!database) res.status(404).send('Error fetching database')
+    if (!database) res.status(404).send('Error fetching database');
     res.send(database);
 })
 
