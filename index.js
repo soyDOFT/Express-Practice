1787a845bf8964751c262b01e5338e2745b20f4f
David Flores <soydoft@gmail.com>
2024-06-06 16:53:30 -0400

    Add comments and clean up code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
+const express = require('express'); //imports express module
+const app = express(); // starts express
+const PORT = process.env.PORT || 8080; // Decides port based on environment variables or sets to default
+//Mock database
 const database = [
     {
         'id': 1,
@@ -19,36 +20,40 @@ const bodyParser = require('body-parser');
 app.use(express.json()); //enable JSON parse in request body
 app.use(bodyParser.json());
 
-app.get('/api/data', (req, res) => {
-    if (!database) res.status(404).send('Error getting database');
-    setTimeout(() => res.send(database), 1000);
+//Handle get http requests
+app.get('/api/data', (req, res) => { //Handler function
+    if (!database) res.status(404).send('Error getting database'); // error handling
+    setTimeout(() => res.send(database), 1000); // Send response back after delay
 })
 
-app.post('/api/data', (req, res) => {
-    const data = req.body;
-    if (!data) res.status(404).send('Error getting data');
-    database.push(data);
-    setTimeout(() => res.send(database), 1000);
+//Handle get http requests
+app.post('/api/data', (req, res) => { //Handler function
+    const data = req.body; // get data from postman
+    if (!data) res.status(404).send('Error getting data'); // error handling
+    database.push(data); //add data to database
+    setTimeout(() => res.send(database), 1000); //send response back after delay
 })
 
-app.put('/api/data/:element', (req, res) => {
+//Handle get http requests
+app.put('/api/data/:element', (req, res) => { //Handler function
+    //Find index of request in database
     const index = database.findIndex((obj) => {
         return obj.id === parseInt(req.params.element, 10);
     });
-    console.log(index);
-    if (index === -1) res.status(404).send('data does not exist');
-    database[index] = req.body;
-    setTimeout(() => res.send(database), 1000);
+    if (index === -1) res.status(404).send('data does not exist'); //error handling
+    database[index] = req.body; //replace data with request body
+    setTimeout(() => res.send(database), 1000); //send response back after delay
 })
 
-app.delete('/api/data/:element', (req, res) => {
+//Handle get http requests
+app.delete('/api/data/:element', (req, res) => { //Handler function
+    //Find index of request
     const index = database.findIndex((obj) => {
         return obj.id === parseInt(req.params.element, 10);
     });
-    console.log(index)
-    if (index === -1) res.status(404).send('data does not exist');
-    database.splice(index, 1);
-    setTimeout(() => res.send(database), 1000);
+    if (index === -1) res.status(404).send('data does not exist'); //error handling
+    database.splice(index, 1); //remove data from response
+    setTimeout(() => res.send(database), 1000); //send response back
 })
-
+//Add listener to express app
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
